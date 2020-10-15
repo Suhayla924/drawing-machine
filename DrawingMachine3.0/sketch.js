8ed783eda36e4e15f5fd19bc7071b8e67dd5a5cd
Suhayla924 <suhaylabhassan@gmail.com>
2020-10-15 14:05:50 -0700

    adding structure

--- DrawingMachine3.0/sketch.js
+++ DrawingMachine3.0/sketch.js
@@ -11,7 +11,7 @@ function draw() {
   if (mouseIsPressed) {
     //ellipse(mouseX, mouseY, 10, 10);
     background(0);
-    stroke(map(mouseX, 0, 600, 160, 10, 25, true), map(mouseY, 600, 0, 160, 10, 25, true));
+    stroke(map(mouseX, 0, 600, 0, 237, true), map(mouseY, 600, 0, 139, 0, true));
     strokeWeight(map(mouseX, 0, 600, 0, 50, true));
     //line(mouseX, mouseY, pmouseX, pmouseY);
     //  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
@@ -27,14 +27,15 @@ function keyTyped() {
   } else if (key === 'd') {
     //display image
     background(255);
-
 beginShape();
     for (let i = 0; i < array.length; i++){
     //  line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
     curveVertex(array[i][0], array[i][1]);
     }
     endShape();
-
+  } else if (key === 'c'){
+    ellipse(mouseX, mouseY, 10, 10);
+    stroke(map(mouseX, 0, 600, 0, 237, true), map(mouseY, 600, 0, 139, 0, true));
   }
   return false;
 }
