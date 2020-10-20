e0fea28a29d2b51bf1e11d7c599a7ab18eab3c83
Suhayla924 <suhaylabhassan@gmail.com>
2020-10-20 13:35:27 -0700

    new stuff

--- DrawingMachine3.1/sketch.js
+++ DrawingMachine3.1/sketch.js
@@ -1,20 +1,23 @@
 let array = [];
+let backgroundColor = 200;
 
 function setup() {
-  createCanvas(600, 600);
-  background(255);
+  createCanvas(windowWidth, windowHeight );
+  background(backgroundColor);
   noFill();
+  strokeWeight(4);
 }
 
 function draw() {
   //(mouseIsPressed == false) is the same as (!mouseIsPressed)
   if (mouseIsPressed) {
-    //ellipse(mouseX, mouseY, 10, 10);
-    background(0);
-    stroke(map(mouseX, 0, 600, 0, 237, true), map(mouseY, 600, 0, 139, 0, true));
-    strokeWeight(map(mouseX, 0, 600, 0, 50, true));
-    //line(mouseX, mouseY, pmouseX, pmouseY);
-    //  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    // the "--" decreases as the "++" increases
+    //"-=" is going to decrease backgroundColor by 2
+    backgroundColor -= 4;
+    background(backgroundColor);
+    stroke(map(mouseX, 0, 600, 0, 237, true),(0,mouseY, 600, 0, 237, true));
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
     array.push([mouseX, mouseY])
   }
 }
@@ -27,18 +30,20 @@ function keyTyped() {
   } else if (key === 'd') {
     //display image
     background(255);
-beginShape();
-    for (let i = 0; i < array.length; i++){
-    //  line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
-    curveVertex(array[i][0], array[i][1]);
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
+      //  line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1]);
     }
     endShape();
-  } else if (key === 'c'){
+  } else if (key === 'c') {
     ellipse(mouseX, mouseY, 10, 10);
     stroke(map(mouseX, 0, 600, 0, 237, true), map(mouseY, 600, 0, 139, 0, true));
   }
   return false;
 }
 
-//not a function is a true or false system variable
-//if mouse is pressed= true if not pressed = false
+function mousePressed(){
+  array = [];
+  backgroundColor= 255;
+}
